8696c1139cdb58fe1dc394b2bad49ce0401a18f9
KevinHa48 <kh220kh@gmail.com>
2021-02-18 23:17:43 -0500

    pain

--- fa_stub.ml
+++ fa_stub.ml
@@ -34,7 +34,7 @@ let a2 = {states = ["q0";"q1";"q2";"q3";"q4"];
           final= ["q2"]
          }
 
-let a3 = {states = ["q0";"q1";"q2"];
+let a3 = {states = ["q0";"q1";"q2";"q3"];
          start = "q0";
          tf = [("q0",'a',"q1"); ("q1",'b',"q1"); ("q1",'c',"q2"); ("q2", 'd', "q3")];
          final = ["q2"; "q3"]}
@@ -42,19 +42,43 @@ let a3 = {states = ["q0";"q1";"q2"];
 let a4 = {states = ["q0"];
          start = "q0";
          tf = [("q0",'a',"q0")];
-         final = []}
+         final = ["q0"]}
 
+let deadA = {states = ["q0";"q1";"q2";"q3";"q4"];
+          start = "q0";
+          tf = [("q0",'a',"q1"); ("q1",'b',"q1")
+               ; ("q1",'c',"q2");  ("q3",'a',"q4")];
+          final= ["q2";"q4"]
+         }
+
+let a5 = {states = ["q0"; "q1"; "q2"; "q3";"q4"];
+         start = "q0";
+         tf = [("q0",'a',"q1"); ("q2",'b',"q3")
+         ; ("q3",'c',"q2"); ("q0",'a',"q3"); ("q3",'e',"q4")];
+         final = ["q2"]}
+
+let a6 = {states = ["q0"; "q1"; "q2"; "q3"];
+         start = "q0";
+         tf = [("q0",'a',"q1"); ("q2",'b',"q3")
+         ; ("q3",'c',"q2")];
+         final = ["q1"]}
+         
 let tf_of_a = [("q0",'a',"q1"); ("q1",'b',"q1"); ("q1",'c',"q2")]
 
-let nfa = {states = ["q0";"q1";"q2";"q3"];
+let nfa = {states = ["q0";"q1";"q2";"q3";"q4"];
          start = "q0";
-         tf = [("q0",'a',"q1"); ("q0", 'a', "q3"); ("q1",'b',"q1"); ("q1",'c',"q2")];
+         tf = [("q0",'a',"q1"); ("q0", 'a', "q3"); ("q1",'b',"q1"); ("q1",'c',"q2"); ("q2",'d',"q4")];
          final = ["q2";"q3"]}
 
-let nfa2 = {states = ["q0";"q1";"q2";"q3"; "q4"];
-         start = "q1";
+let nfa2 = {states = ["q0";"q1";"q2";"q3";"q4"];
+         start = "q0";
          tf = [("q0",'a',"q1"); ("q0", 'a', "q3"); ("q0", 'a', "q4"); ("q1",'b',"q1"); ("q1",'c',"q2"); ("q1",'b',"q3")];
          final = ["q2";"q3"; "q4"]}
+
+let nfa3 = {states = ["q0";"q1";"q2";"q3";"q4"; "q5"; "q6"];
+         start = "q0";
+         tf = [("q0",'a',"q1"); ("q0", 'a', "q2"); ("q1", 'b', "q3"); ("q2",'c',"q4"); ("q3",'d',"q5"); ("q4",'e',"q6")];
+         final = ["q5";"q6"]}
 
 
 (* ******************************************** *)
@@ -70,6 +94,18 @@ let extract i =
   match i with
   | Some i -> i
   | None -> ""
+
+let rec rem_extra l =
+  match l with
+  | [] -> []
+  | h::tl ->
+  if List.mem h tl
+  then rem_extra tl
+  else h::rem_extra tl
+
+let dead_states_helper l = 
+  failwith "implement"
+
 
 (* ******************************************** *)
 (* Simulating automata *)
@@ -124,17 +160,35 @@ let valid (finAuto: fa) : bool =
     List.mem h finAuto.states && subset tl
   in
   subset (finAuto.start::finAuto.final) && deterministic finAuto
-  
+
 let reachable (finAuto: fa) : state list =
-  let rec reachHelp l =
+  let rec directReach (st: state) (l: tf) =
     match l with 
     | [] -> []
-    | (q, s, f)::tl ->
-    if q = finAuto.start 
-    then f::[] @ reachHelp tl
-    else reachHelp tl
+    | (q, _, f)::tl ->
+    if q = st && q <> f
+    then f::[] @ directReach st tl
+    else directReach st tl
   in
-  reachHelp finAuto.tf
+  let rec search (s: state list) =
+    match s with
+    | [] -> []
+    | h::tl ->
+    directReach h finAuto.tf @ search (directReach h finAuto.tf) @ search tl
+  in
+  let start = directReach finAuto.start finAuto.tf in
+  rem_extra (start @ search start)
+
+let remove_dead_states (finAuto: fa) : fa =
+  let fa_cleaned = {
+    states = dead_states_helper finAuto.states;
+    start = finAuto.start;
+    tf = dead_states_helper finAuto.tf;
+    final = dead_states_helper finAuto.final;
+  }
+  in
+  fa_cleaned
+
 
 
 (*
@@ -155,5 +209,3 @@ of the automata's state list.
 3. And that bool with determinstic.
 
 *)
-
-(* TODO: Ask about no final states... *)
